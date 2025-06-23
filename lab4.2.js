0fcc5007a616ea8933035bdbd0714dc9fe4420c7
Daniel Ward <kingsgambit35@gmail.com>
2025-06-23 15:08:50 -0400

    taskadd

--- lab4.2.js
+++ lab4.2.js
@@ -2,5 +2,48 @@ function callcenter() {
     console.log(5)
 }
 
-callcenter();
+const shoppinglist = [];
 
+function addItem() {
+    shoppinglist.pop()
+}
+
+let taskInput = document.getElementById("taskInput");
+let addTaskButton = document.getElementById("addTaskButton");
+let taskList = document.getElementById("taskList");
+
+addTaskButton.addEventListener("click", function () {
+    let taskText = taskInput.value;
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+
+    let listItem = document.createElement("li");
+
+
+    listItem.innerText = taskText;
+    listItem.contentEditable = "true";
+
+    let input = document.createElement("input");
+    input.type = "checkbox";
+
+    listItem.appendChild(input);
+
+    let mrButton = document.createElement("button");
+    mrButton.innerText = "Press";
+
+    mrButton.addEventListener("click", function (event) {
+        if (event.target.parentElement.tagName === "LI") {
+            taskList.removeChild(event.target.parentElement);
+        }
+    });
+
+    listItem.appendChild(mrButton);
+
+    taskList.appendChild(listItem);
+
+    taskInput.value = "";
+});
